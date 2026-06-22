a58bf4a99db9532addaf2d018572e55490894ef5
igorkartasovass <igor.kartasovas7@gmail.com>
2026-06-21 17:31:14 -0700

    added some style to home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function HomeScreen() {
+  const [taskText, setTaskText] = useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>My Tasks</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Insert tasks here...?"
+        placeholderTextColor="#000000"
+        value={taskText}
+        onChangeText={setTaskText}
+      ></TextInput>
     </View>
   );
 }
@@ -13,11 +23,22 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 80,
     paddingHorizontal: 20,
-    backgroundColor: "#fff",
+    backgroundColor: "#6ea2e3",
   },
   title: {
-    fontSize: 28,
+    fontSize: 40,
+    color: "#2822e1",
+    fontFamily: "Monospace",
     fontWeight: "bold",
     marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1.5,
+    borderColor: "#2822e1",
+    borderRadius: 30,
+    padding: 12,
+    fontSize: 16,
+    color: "#000000",
+    marginTop: 20,
   },
 });
